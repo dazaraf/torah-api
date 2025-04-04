4647de3ab9ee576688d95b390715f60997db8776
David Azaraf <dazaraf@gmail.com>
2025-04-04 13:49:30 +0300

    fixed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 echo "Starting on port $PORT"
 #!/bin/bash
-gunicorn torah_api:app --bind 0.0.0.0:$PORT --timeout 120 --workers 2 --threads 4
+gunicorn torah-api:app --bind 0.0.0.0:$PORT --timeout 120 --workers 2 --threads 4
 chmod +x start.sh
